9fbd5e98a45cc7777dffd9573d68151ce69512b1
Felipe Paz <felipepaz48@gmail.com>
2023-07-17 19:42:28 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,2 @@
+
+figura = parseInt (prompt("A continuacion se muestran una lista de figuras para calcular su volumen, por favor elija la de su interes:\n 1- Cubo\n 2 - Prisma Rectangular\n 3 - Cilindro\n 4 - Esfera\n 5- Cono\n"));
