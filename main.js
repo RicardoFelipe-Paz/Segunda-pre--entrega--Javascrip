9392f9f930794c8c601bd2446595de0036c9513f
Felipe Paz <felipepaz48@gmail.com>
2023-07-17 23:40:51 -0300

    tercer commit.

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
 
-figura = parseInt (prompt("A continuacion se muestran una lista de figuras para calcular su volumen, por favor elija la de su interes:\n 1- Cubo\n 2 - Prisma Rectangular\n 3 - Cilindro\n 4 - Esfera\n 5- Cono\n"));
+const pi = 3.141592;
+const tercio = 0.33333;
+
+let repetir = 1;
+let base = 0;
+let ancho = 0;
+let altura = 0;
+let radio = 0;
+let figura = 0;
+let volumen = 0;
+let cuerpo = null;
+
+cuerpo = prompt("A continuacion se muestran una lista de figuras para calcular su volumen, por favor elija la de su interes:\n 1- Cubo\n 2 - Prisma \n 3 - Cilindro\n 4 - Esfera\n 5- Cono\n");
+cuerpo = cuerpo.toLowerCase();
+
+console.log ("El volumen calculado es: " + calcularvoloumen (cuerpo) + " m3");
+console.log 
 
 function calcularvoloumen (cuerpo) {
 
-    if (cuerpo == 1) {
-        base = prompt ("Ingrese el valor de base del cubo:\nRecuerde que los valores deben ser expresados en metros");
-        return base**3;
+    if (cuerpo == "cubo") { //Cubo
+
+       base = prompt ("Ingrese el valor de base del cubo:\nRecuerde que los valores deben ser expresados en metros");
+        return (base) => base**3;
     } 
-    else if  (cuerpo == 2) {
+    else if  (cuerpo == "prisma") { //Prisma rectangular
 
         base = prompt ("Por favor ingrese el valor de base del Prisma: \nRecuerde que los valores deben ser expresados en metros");
         ancho = prompt ("Ahora ingrese el valor de ancho: \n");
         altura = prompt ("Por ultimo ingrese el valor de altura: \n");
-        volumen = base*ancho*altura;
-
+        return (base, ancho, altura) => base*ancho*altura;
     }
-    else if (cuerpo == 3){
-        radio = prompt ("Ingrese el valor del radio de la circunferencia: \nRecuerde que los valores deben ser expresados en metros");
-        altura = prompt ("Ahora ingrese la altura del cilindro: \n");
-        volumen = pi*radio**2*altura;
+    else if (cuerpo == "cilindro"){ //Cilindro
 
+    radio = prompt ("Ingrese el valor del radio de la circunferencia: \nRecuerde que los valores deben ser expresados en metros");
+    altura = prompt ("Ahora ingrese la altura del cilindro: \n");        
+        return (radio, altura) => pi*radio**2*altura;
     }
-    else if (cuerpo == 4){
+    else if (cuerpo == "esfera"){ //Esfera
+        
         radio = prompt ("Ingrese el valor del radio: \nRecuerde que los valores deben ser expresados en metros")
-        volumen = 4*tercio*pi*radio**3;
-    
-
+        return (radio) => 4*tercio*pi*radio**3;
     }
-    else if (cuerpo == 5) {
-        altura = prompt ("Ingrese la altura del cilindro: \nRecuerde que los valores deben ser expresados en metros");
-        radio = prompt ("Ingrese el valor del radio de la base del cono: \n")
-        volumen = tercio*pi*radio**2*altura;
-    
-
+    else if (cuerpo == "cono") { //Cono
+        
+    altura = prompt ("Ingrese la altura del cilindro: \nRecuerde que los valores deben ser expresados en metros");
+    radio = prompt ("Ingrese el valor del radio de la base del cono: \n")
+        return(altura, radio) => tercio*pi*radio**2*altura;
     } else {
-
+        Alert ("Por favor elija un cuerpo de la lista.")
     }
 
 
