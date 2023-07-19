8860f02725e5b9aa7ca204030aff0ed9cccc581e
Felipe Paz <felipepaz48@gmail.com>
2023-07-19 13:04:24 -0300

    No repite whilw. Intentando solucionar

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const pi = 3.141592;
 const tercio = 0.33333;
 
-let repetir = 1;
 let base = 0;
 let ancho = 0;
 let altura = 0;
@@ -21,7 +20,7 @@ do {
     cuerpo = prompt("A continuacion se muestran una lista de figuras para calcular su volumen, por favor elija la de su interes:\n 1- Cubo\n 2 - Prisma \n 3 - Cilindro\n 4 - Esfera\n 5- Cono\n 0- Ver Historial");
     cuerpo = cuerpo.toLowerCase();
    
-    if (cuerpo ==0) {
+    if (cuerpo == "historial") {
         mostrarhistorial();
         
     } else {
